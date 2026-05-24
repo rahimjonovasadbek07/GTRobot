5fbcdbcbdb2bc8fc399ac1afabe7494755dca6aa
Rahimjonov <sizning@email.com>
2026-05-24 20:48:34 +0500

    update config

--- config.py
+++ config.py
@@ -3,10 +3,9 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "837837025").split(",")]
 
-# Majburiy kanallar
 REQUIRED_CHANNELS = [
-    {"id": "-1001908526934", "name": "Crypto Savdosi", "link": "https://t.me/crypto_savdosi"},
-    {"id": "-1002519689075", "name": "IT Studio Uz", "link": "https://t.me/ITstudio_uz"},
+    {"id": "@cryptosavdosi", "name": "Crypto Savdosi"},
+    {"id": "@crypto_savdosi", "name": "Crypto Savdosi 2"},
 ]
 
 MEXC_API_BASE = "https://api.mexc.com"
